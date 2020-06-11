b29c3e18a2640b9eaa54cd330a29125318d0330e
Student Name <rexsutton2@gmail.cogit>
2020-06-11 10:10:40 -0500

    Set up for sessions and cookies exercise

---
+++ src/main/java/Config.java
@@ -0,0 +1,13 @@
+public class Config {
+
+        public String getUrl() {
+            return "jdbc:mysql://localhost/database_name?serverTimezone=UTC&useSSL=false";
+        }
+        public String getUser() {
+            return "someuser";
+        }
+        public String getPassword() {
+            return "strongpassword";
+        }
+    }
+
