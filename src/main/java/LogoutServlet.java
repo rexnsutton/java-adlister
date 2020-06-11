fe9effc18dc219ab781af0a129195e3f786a30e1
Student Name <rexsutton2@gmail.cogit>
2020-06-11 11:38:45 -0500

    finished sessions and cookies exercise

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 
 @WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    request.getSession().invalidate();
-    response.sendRedirect("/login");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("/login");
     }
 }
