7988ba7f18c7520c655fdae43c1290ffcd8fb34f
Student Name <rexsutton2@gmail.cogit>
2020-06-12 10:46:49 -0500

    finished jdbc exercise

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,66 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+
+    private Connection connection = null;
+
+    public MySQLAdsDao(Config config) {
+            try {
+                DriverManager.registerDriver(new Driver());
+                connection = DriverManager.getConnection(
+                        config.getUrl(),
+                        config.getUser(),
+                        config.getPassword()
+                );
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    @Override
+    public List<Ad> all() {
+
+        List<Ad> ads = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from ads");
+            while(rs.next()){
+                // translating the rs into a java obj
+                ads.add(new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        String query = String.format("insert into ads (user_id, title, description) values (%s, '%s', '%s')", ad.getUserId(), ad.getTitle(), ad.getDescription());
+
+        System.out.println("query = " + query);
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return null;
+    }
+}
